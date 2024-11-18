37782c1930b3f9a38287a2fe7443dc2ccb2ad1e1
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2024-11-18 16:33:42 +0100

    Create ModificaComponentePage.jsx

---
+++ client/src/pages/dashpages/ModificaComponentePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Footer from '../../Components/Common/Footer';
+import Navbar from '../../Components/Common/DashNav';
+import ModificaComponente from '../../Components/dashcomponents/ModificaComponente';
+
+
+function ModificaComponentePage() {
+  return (
+    <>
+    <Navbar />
+    <ModificaComponente />
+    <Footer />
+    </>
+  )
+}
+
+export default ModificaComponentePage
+
