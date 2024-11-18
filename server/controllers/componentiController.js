e12b7d514fca8a5b01bdf61d2ac01d88317ad4e6
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2024-11-18 16:30:14 +0100

    Update componentiController.js

--- server/controllers/componentiController.js
+++ server/controllers/componentiController.js
@@ -15,9 +15,23 @@ const vediComponenti = async (req, res)=> {
 
 }
 
+// Get all Componenti: OK
+const vediSingoloComp = async (req, res)=> {
+
+    console.log("Req from /")
+    console.log("View all Componenti")
+
+    const allComponenti = await Componenti.find({}).sort({createdAt: -1});
+    res.status(200).json(allComponenti)
+
+}
+
+
+
 
 
 
 module.exports = {
-        vediComponenti
+        vediComponenti,
+		vediSingoloComp
 }
