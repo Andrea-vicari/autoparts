c045464ce3162001b2829fe1eeb28221b84f10b0
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2024-11-18 16:32:46 +0100

    Update NuovoComponente.jsx

--- client/src/Components/dashcomponents/NuovoComponente.jsx
+++ client/src/Components/dashcomponents/NuovoComponente.jsx
@@ -18,10 +18,20 @@ const NuovoComponente = () => {
   themeType == "ligth" ? textType = "" : textType = "text-bg-dark"
 
      const [nome, setNome] = useState()
-     const [descrizione, setDescription] = useState()
+     const [descrizione, setDescrizione] = useState()
      const [codUni, setCodUni] = useState('')
+	 const [campata, setCampata] = useState('')
+	 const [codice, setCodice] = useState('')
+	 const [condizione, setCondizione] = useState('')
+	 const [peso, setPeso] = useState('')
      const [categoria, setCategoria] = useState('')
+	 const [scaffale, setScaffale] = useState('')
+	 const [ripiano, setRipiano] = useState('')
+	 const [cassetta, setCassetta] = useState('')
      const [annoImmatricolazione, setAnnoImmatricolazione] = useState('')
+	 const [marca, setMarca] = useState('')
+	 const [modello, setModello] = useState('')
+	 const [versione, setVersione] = useState('')
      const [file, setFile] = useState()
      const navigate = useNavigate()
 
@@ -35,7 +45,17 @@ const NuovoComponente = () => {
         formData.append('descrizione', descrizione),
         formData.append('categoria', categoria),
         formData.append('annoImmatricolazione', annoImmatricolazione),
-        formData.append('file', file)
+		formData.append('codice', codice),
+		formData.append('condizione', condizione),		
+        formData.append('file', file),
+		formData.append('peso', peso),
+		formData.append('scaffale', scaffale),
+		formData.append('campata', campata),
+		formData.append('ripiano', ripiano),
+		formData.append('cassetta', cassetta),
+		formData.append('marca', marca),
+		formData.append('modello', modello),
+		formData.append('versione', modello),		
         formData.append('unicoID', unicoID)
 
         axios.post('http://localhost:8080/api/componenti', formData)
@@ -53,7 +73,7 @@ const NuovoComponente = () => {
 
       <div className="d-flex justify-content-center align-items-center py-5">
         <div className={"p-3 rounded w-100 " + bgType + textType}>
-          <h2 className={textType}>Aggiungi nuovo prodotto</h2>
+          <h2 className={textType}>Aggiungi nuovo componente</h2>
           <form onSubmit={handleSubmit}>
             <div className="row g-3">
             <div className="mb-3 col-md-4">
@@ -71,7 +91,22 @@ const NuovoComponente = () => {
                   required={true}
                 />
             </div>
-            <div className="mb-3 col-md-4">
+			<div className="mb-3 col-md-3">
+                <label htmlFor="descrizione" className="text-primary">
+                  <strong>Descrizione Prodotto</strong>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Inserisci descrizione Prodotto"
+                  autoComplete="off"
+                  name="text"
+                  className="form-control rounded-0"
+                  onChange={(e) => setDescrizione(e.target.value)}
+                  value={descrizione}
+                  required={true}
+                />
+              </div>
+			<div className="mb-3 col-md-4">
                 <label htmlFor="categoria" className="text-primary">
                   <strong>Categoria Prodotto</strong>
                 </label>
@@ -87,7 +122,112 @@ const NuovoComponente = () => {
                   required={true}
                 />
               </div>
-              <div className="mb-3 col-md-4">
+			<div className="mb-3 col-md-4">
+                <label htmlFor="codice" className="text-primary">
+                  <strong>Codice Prodotto</strong>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Inserisci codice Prodotto"
+                  autoComplete="off"
+                  name="text"
+                  className="form-control rounded-0"
+                  onChange={(e) => setCodice(e.target.value)}
+                  value={codice}
+                  required={true}
+                />
+              </div>
+			  <div className="mb-3 col-md-4">
+                <label htmlFor="condizione" className="text-primary">
+                  <strong>Condizione Prodotto</strong>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Inserisci condizione Prodotto"
+                  autoComplete="off"
+                  name="text"
+                  className="form-control rounded-0"
+                  onChange={(e) => setCondizione(e.target.value)}
+                  value={condizione}
+                  required={true}
+                />
+              </div>
+			  <div className="mb-3 col-md-4">
+                <label htmlFor="peso" className="text-primary">
+                  <strong>Peso Prodotto</strong>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Inserisci peso Prodotto"
+                  autoComplete="off"
+                  name="text"
+                  className="form-control rounded-0"
+                  onChange={(e) => setPeso(e.target.value)}
+                  value={peso}
+                  required={true}
+                />
+              </div>
+			  <div className="mb-3 col-md-4">
+                <label htmlFor="scaffale" className="text-primary">
+                  <strong>Scaffale</strong>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Inserisci scaffale"
+                  autoComplete="off"
+                  name="text"
+                  className="form-control rounded-0"
+                  onChange={(e) => setScaffale(e.target.value)}
+                  value={scaffale}
+                  required={true}
+                />
+              </div>
+			  <div className="mb-3 col-md-4">
+                <label htmlFor="campata" className="text-primary">
+                  <strong>Campata</strong>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Inserisci campata"
+                  autoComplete="off"
+                  name="text"
+                  className="form-control rounded-0"
+                  onChange={(e) => setCampata(e.target.value)}
+                  value={campata}
+                  required={true}
+                />
+              </div>
+			  <div className="mb-3 col-md-6">
+                <label htmlFor="ripiano" className="text-primary">
+                  <strong>Ripiano</strong>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Inserisci ripiano"
+                  autoComplete="off"
+                  name="text"
+                  className="form-control rounded-0"
+                  onChange={(e) => setRipiano(e.target.value)}
+                  value={ripiano}
+                  required={true}
+                />
+              </div>
+			  <div className="mb-3 col-md-4">
+                <label htmlFor="cassetta" className="text-primary">
+                  <strong>Cassetta</strong>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Inserisci cassetta"
+                  autoComplete="off"
+                  name="text"
+                  className="form-control rounded-0"
+                  onChange={(e) => setCassetta(e.target.value)}
+                  value={cassetta}
+                  required={true}
+                />
+              </div>
+			  <div className="mb-3 col-md-4">
                 <label htmlFor="annoImmatricolazione" className="text-primary">
                   <strong>Anno Immatricolazione</strong>
                 </label>
@@ -102,6 +242,53 @@ const NuovoComponente = () => {
                   required={true}
                 />
               </div>
+			  <div className="mb-3 col-md-4">
+                <label htmlFor="marca" className="text-primary">
+                  <strong>Marca</strong>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Inserisci marca"
+                  autoComplete="off"
+                  name="text"
+                  className="form-control rounded-0"
+                  onChange={(e) => setMarca(e.target.value)}
+                  value={marca}
+                  required={true}
+                />
+              </div>
+			  <div className="mb-3 col-md-6">
+                <label htmlFor="modello" className="text-primary">
+                  <strong>Modello</strong>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Inserisci Modello"
+                  autoComplete="off"
+                  name="text"
+                  className="form-control rounded-0"
+                  onChange={(e) => setModello(e.target.value)}
+                  value={modello}
+                  required={true}
+                />
+              </div>
+			  <div className="mb-3 col-md-4">
+                <label htmlFor="versione" className="text-primary">
+                  <strong>Versione</strong>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Inserisci Versione"
+                  autoComplete="off"
+                  name="text"
+                  className="form-control rounded-0"
+                  onChange={(e) => setVersione(e.target.value)}
+                  value={versione}
+                  required={true}
+                />
+              </div>
+            
+              
             </div>
 
           <input type="file" className="file" placeholder="Select File"
