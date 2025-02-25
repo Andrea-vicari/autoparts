87fba74d9a013ce04bd6618d7b97b6a5540e702c
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-02-25 14:39:02 +0100

    Update AppFilt.jsx

--- client/src/AppFilt.jsx
+++ client/src/AppFilt.jsx
@@ -1,1 +1,23 @@
+import { useState } from 'react'
 
+function AppFilt() {
+  const [searchItem, setSearchItem] = useState('')
+
+  const handleInputChange = (e) => { 
+    const searchTerm = e.target.value;
+    setSearchItem(searchTerm)
+  }
+
+  return (
+    <div>      
+      <input
+        type="text"
+        value={searchItem}
+        onChange={handleInputChange}
+        placeholder='Type to search'
+      />
+    </div>
+  )
+}
+
+export default AppFilt
