e1be4849526a4add80295d3b3addf5411c6a7dca
Andrea Vicari <andrea.vicari77@gmail.com>
2025-03-17 22:20:11 +0100

    RAVA PARK SERA
    NuovoFiltro.jsx: Funziona filtro con Marca Modello Nome e la paginazione. Ora va sistemato il layout con Bootstrap GRID

--- client/src/Components/dashcomponents/NuovoFiltro.jsx
+++ client/src/Components/dashcomponents/NuovoFiltro.jsx
@@ -28,6 +28,9 @@ function NuovoFiltro() {
   const [modelloFilter, setModelloFilter] = useState('');
   const [nomeFilter, setNomeFilter] = useState('');
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, SetPostsPerPage] = useState(2);
+
   useEffect(() => {
     fetch('http://localhost:8080/api/componenti')
       .then(response => response.json())
@@ -56,6 +59,17 @@ function NuovoFiltro() {
     product.nome.toLowerCase().includes(nomeFilter.toLowerCase())
   );
 
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = filteredProducts.slice(indexOfFirstPost, indexOfLastPost);
+
+  console.log("currentPosts")
+  console.log(currentPosts)
+
+  const handlePagination = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  }
+
   return (
     <>
           <div className='container-fluid pt-1 mt-5 bg-login'>
@@ -65,33 +79,77 @@ function NuovoFiltro() {
         </div>
         </div>
 
-      <div className="container mt-t pt-5">
+      <div className="container-fluid mt-2">
+        <h5 className="mx-2 mb-1">Filtra</h5>
+        <div className="row mb-3">
+        <div className="col-sm-4 themed-grid-col">.col-sm-4</div>
+        <div className="col-sm-4 themed-grid-col">.col-sm-4</div>
+        <div className="col-sm-4 themed-grid-col">.col-sm-4</div>
+      </div>
+        <div>
         <input
+          className="mb-2 mx-2"
           type="text"
           placeholder="Inserisci la marca"
           value={marcaFilter}
           onChange={(e) => setMarcaFilter(e.target.value)}
         />
         <input
+          className="mb-2 mx-2"
           type="text"
           placeholder="Inserisci il modello"
           value={modelloFilter}
           onChange={(e) => setModelloFilter(e.target.value)}
         />
         <input
+          className="mb-2 mx-2"
           type="text"
           placeholder="Inserisci il nome del componente"
           value={nomeFilter}
           onChange={(e) => setNomeFilter(e.target.value)}
         />
+
+        </div>
+        <div>
+        <Link to="/nuovocomponente" type="button" className="btn btn-success">
+            <i className="bi bi-plus-circle mx-2">
+            </i>Aggiungi Componente
+          </Link>
+        </div>
+
+
+
+
       </div>
-      <ul>
-        {filteredProducts.map((product) => (
-          <li key={product.unicoID}>
-            {product.nome}
-          </li>
-        ))}
-      </ul>
+          <section className={"py-3" + " " + bgType + " " + textType}>
+        <div className="container-fluid mt-2 pt-0">
+          <div className='table-responsive-lg mt-2'>
+            <table className={"table table-striped table-hover" + " " + tableType}>
+              <thead className='text-uppercase'>
+                <tr>
+                  <th scope="col">Immagine</th>
+                  <th scope="col">Codice</th>
+                  <th scope="col">Nome</th>
+                  <th scope="col">Cat</th>
+                  <th scope="col">Condizione</th>
+                  <th scope="col">Marca</th>
+                  <th scope="col">Modello</th>
+                  <th scope="col">Versione</th>
+                  <th scope="col">Anno</th>
+                  <th scope="col">Ubicazione</th>
+                  <th scope="col">Peso</th>
+                  <th scope="col">Gestisci</th>
+                </tr>
+              </thead>
+              <tbody>
+                <Lista componenti={currentPosts} loading={loading} />
+              </tbody>
+            </table>
+            <Pagination length={componenti.length} postsPerPage={postsPerPage} handlePagination={handlePagination} currentPage={currentPage} />
+          </div>
+        </div>
+
+    </section>
     </>
   );
 }
