15d9fa70267f9dbe2e8c696e753a39d674f88db4
Andrea Vicari <andrea.vicari77@gmail.com>
2025-03-26 22:05:44 +0100

    RAVA PARK SERA
    Per sistemare le immagini che non si vedono
    app.use(express.static(path.join(__dirname, 'public')));

--- server/server.js
+++ server/server.js
@@ -11,11 +11,11 @@ const path = require('path');
 app.use(express.json());
 
 
-app.use(express.static('public'))
+//app.use(express.static('public'))
 
 //app.use(express.static(path.join(process.cwd(), 'public')))
 //app.use('public', express.static(path.join(__dirname,'public')));
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 console.log(process.cwd())
 
