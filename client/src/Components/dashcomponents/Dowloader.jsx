6a0a11f5081fb91ac6ebd8f38e8998f4f9e7d3b0
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-03-28 16:17:53 +0100

    Update Dowloader.jsx

--- client/src/Components/dashcomponents/Dowloader.jsx
+++ client/src/Components/dashcomponents/Dowloader.jsx
@@ -1,44 +1,51 @@
 import React from 'react'
 
 
-const downloadFile = ({ data, fileName, fileType }) => {
-  const blob = new Blob([data], { type: fileType })
-  const a = document.createElement('a')
-  a.download = fileName
-  a.href = window.URL.createObjectURL(blob)
-  const clickEvt = new MouseEvent('click', {
-    view: window,
-    bubbles: true,
-    cancelable: true,
-  })
-  a.dispatchEvent(clickEvt)
-  a.remove()
-}
-const exportToJson = e => {
-  e.preventDefault()
-  downloadFile({
-    data: JSON.stringify(usersData.users),
-    fileName: 'users.json',
-    fileType: 'text/json',
-  })
-}
-const exportToCsv = e => {
-  e.preventDefault()
-  // Headers for each column
-  let headers = ['Id,Name,Surname,Age']
-  // Convert users data to a csv
-  let usersCsv = usersData.users.reduce((acc, user) => {
-    const { id, name, surname, age } = user
-    acc.push([id, name, surname, age].join(','))
-    return acc
-  }, [])
-  downloadFile({
-    data: [...headers, ...usersCsv].join('\n'),
-    fileName: 'users.csv',
-    fileType: 'text/csv',
-  })
-}
 function Dowloader() {
+
+      const downloadFile = ({ data, fileName, fileType }) => {
+      const blob = new Blob([data], { type: fileType })
+      const a = document.createElement('a')
+      a.download = fileName
+      a.href = window.URL.createObjectURL(blob)
+      const clickEvt = new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true,
+      })
+      a.dispatchEvent(clickEvt)
+      a.remove()
+    }
+    
+    // Cambia UserData
+    const exportToJson = e => {
+      e.preventDefault()
+      downloadFile({
+        data: JSON.stringify(usersData.users),
+        fileName: 'users.json',
+        fileType: 'text/json',
+      })
+    }
+    const exportToCsv = e => {
+      e.preventDefault()
+      // Headers for each column
+      let headers = ['Id,Name,Surname,Age']
+      // Convert users data to a csv
+      let usersCsv = usersData.users.reduce((acc, user) => {
+        const { id, name, surname, age } = user
+        acc.push([id, name, surname, age].join(','))
+        return acc
+      }, [])
+      downloadFile({
+        data: [...headers, ...usersCsv].join('\n'),
+        fileName: 'users.csv',
+        fileType: 'text/csv',
+      })
+    }
+
+
+
+  
   return (
     <div className='App'>
       <h1>How to download CSV and JSON files in React</h1>
