e4358bfbb78b4552cde3680374ab05c7f7ee8a9c
Andrea Vicari <andrea.vicari77@gmail.com>
2025-03-28 22:11:15 +0100

    Aggiunti campi al reducer del CSV

--- client/src/Components/dashcomponents/Downloader.jsx
+++ client/src/Components/dashcomponents/Downloader.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-
-
 function Downloader({ componenti }) {
 
       var dataCreazioneFileCSV = new Date().toDateString()
@@ -21,18 +19,20 @@ function Downloader({ componenti }) {
       a.remove()
     }
 
-    const exportToCsv = e => {
+    const esportaCsv = e => {
       e.preventDefault()
       // Headers for each column
       let headers = ['Nome']
-      // Convert users data to a csv
-      let usersCsv = componenti.reduce((acc, user) => {
-        const { nome } = user
-        acc.push([nome].join(','))
+
+      let listaCsv = componenti.reduce((acc, componentiLista) => {
+        {/* const { id, name, surname, age } = user */}
+        const { nome, categoria, codice, condizione, peso, scaffale, campata, ripiano, cassetta, modello, versione, anno, unicoID } = componentiLista
+        {/* acc.push([id, name, surname, age].join(',')) */}
+        acc.push([nome, categoria, codice, condizione, peso, scaffale, campata, ripiano, cassetta, modello, versione, anno, unicoID].join(','))
         return acc
       }, [])
       downloadFile({
-        data: [...headers, ...usersCsv].join('\n'),
+        data: [...headers, ...listaCsv].join('\n'),
         fileName: 'ListaComponenti' + "-" + dataCreazioneFileCSV + '.csv',
         fileType: 'text/csv',
       })
@@ -40,7 +40,7 @@ function Downloader({ componenti }) {
   return (
     <>
 
-        <button onClick={exportToCsv} type="button" className="btn btn-outline-success rounded-0">
+        <button onClick={esportaCsv} type="button" className="btn btn-outline-success rounded-0">
         <i className="bi bi-file-earmark-arrow-down mx-2">
         </i>Esporta Lista CSV
         </button>
