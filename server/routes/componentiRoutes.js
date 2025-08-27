bb66b869e5092ca99f1825a3d395085f0e67bc8a
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-08-27 22:18:08 +0200

    Update componentiRoutes.js

--- server/routes/componentiRoutes.js
+++ server/routes/componentiRoutes.js
@@ -8,6 +8,7 @@ const router = express.Router();
 async function handleUpload(file) {
 
     const res = await cloudinary.uploader.upload(file, {
+	folder:"Autoparts",
     resource_type: "auto",
     });
     console.log(res)
