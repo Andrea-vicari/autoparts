e6bcf02619f0c867dcc177c9d5b0af192132a5c7
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2026-02-05 15:16:23 +0100

    Update veicoliModel.js

--- server/models/veicoliModel.js
+++ server/models/veicoliModel.js
@@ -2,46 +2,6 @@ const mongoose = require('mongoose')
 
 const veicoliSchema = new mongoose.Schema({
 
-    nome:{
-        type: String,
-        required: false
-    },
-    categoria:{
-        type: String,
-        required: false
-    },
-    descrizione:{
-        type: String,
-        required: false
-    },
-    codice:{
-        type: String,
-        required: false
-    },
-    condizione:{
-        type: String,
-        required: false
-    },
-    peso:{
-        type: Number,
-        required: false
-    },
-    scaffale:{
-        type: Number,
-        required: false
-    },
-    campata:{
-        type: Number,
-        required: false
-    },
-    ripiano:{
-        type: Number,
-        required: false
-    },
-    cassetta:{
-        type: Number,
-        required: false
-    },
     marca:{
         type: String,
         required: false
