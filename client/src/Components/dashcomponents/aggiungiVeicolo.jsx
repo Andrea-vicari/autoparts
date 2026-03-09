0116b4b0440d757850d1a4ecb4fe7f7c46c51534
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2026-03-09 14:19:18 +0100

    Update aggiungiVeicolo.jsx

--- client/src/Components/dashcomponents/aggiungiVeicolo.jsx
+++ client/src/Components/dashcomponents/aggiungiVeicolo.jsx
@@ -44,172 +44,9 @@ function AggiungiVeicolo({ urlImmagine }) {
   return (
     <form onSubmit={handleSubmit} className='mt-5'>
     <div className="row g-3">
-    <div className="mb-3 col-md-4">
-    <label htmlFor="nome" className="text-primary">
-          <strong>Nome Componente</strong>
-    </label>
-    <input
-          type="text"
-          placeholder="Inserisci nome Componente"
-          autoComplete="off"
-          name="text"
-          className="form-control rounded-0"
-          onChange={(e) => setNome(e.target.value)}
-          value={nome}
-          required={true}
-        />
-    </div>
-    <div className="mb-3 col-md-3">
-        <label htmlFor="descrizione" className="text-primary">
-          <strong>Descrizione Componente</strong>
-        </label>
-        <input
-          type="text"
-          placeholder="Inserisci descrizione Componente"
-          autoComplete="off"
-          name="text"
-          className="form-control rounded-0"
-          onChange={(e) => setDescrizione(e.target.value)}
-          value={descrizione}
-          required={true}
-        />
-      </div>
-    <div className="mb-3 col-md-4">
-        <label htmlFor="categoria" className="text-primary">
-          <strong>Categoria Componente</strong>
-        </label>
-        <input
-          type="text"
-          placeholder="Inserisci categoria  Componente"
-          autoComplete="off"
-          value={categoria}
-          name="text"
-          className="form-control rounded-0"
-          onChange={(e) => setCategoria(e.target.value)}
 
-          required={true}
-        />
-      </div>
-    <div className="mb-3 col-md-4">
-        <label htmlFor="codice" className="text-primary">
-          <strong>Codice Componente</strong>
-        </label>
-        <input
-          type="text"
-          placeholder="Inserisci codice Componente"
-          autoComplete="off"
-          name="text"
-          className="form-control rounded-0"
-          onChange={(e) => setCodice(e.target.value)}
-          value={codice}
-          required={true}
-        />
-      </div>
-      <div className="mb-3 col-md-4">
-        <label htmlFor="condizione" className="text-primary">
-          <strong>Condizione Componente</strong>
-        </label>
-        <input
-          type="text"
-          placeholder="Inserisci condizione Componente"
-          autoComplete="off"
-          name="text"
-          className="form-control rounded-0"
-          onChange={(e) => setCondizione(e.target.value)}
-          value={condizione}
-          required={true}
-        />
-      </div>
-      <div className="mb-3 col-md-4">
-        <label htmlFor="peso" className="text-primary">
-          <strong>Peso Componente</strong>
-        </label>
-        <input
-          type="number"
-          placeholder="Inserisci peso Componente"
-          autoComplete="off"
-          name="text"
-          className="form-control rounded-0"
-          onChange={(e) => setPeso(e.target.value)}
-          value={peso}
-          required={true}
-        />
-      </div>
-      <div className="mb-3 col-md-4">
-        <label htmlFor="scaffale" className="text-primary">
-          <strong>Scaffale</strong>
-        </label>
-        <input
-          type="text"
-          placeholder="Inserisci scaffale"
-          autoComplete="off"
-          name="text"
-          className="form-control rounded-0"
-          onChange={(e) => setScaffale(e.target.value)}
-          value={scaffale}
-          required={true}
-        />
-      </div>
-      <div className="mb-3 col-md-4">
-        <label htmlFor="campata" className="text-primary">
-          <strong>Campata</strong>
-        </label>
-        <input
-          type="text"
-          placeholder="Inserisci campata"
-          autoComplete="off"
-          name="text"
-          className="form-control rounded-0"
-          onChange={(e) => setCampata(e.target.value)}
-          value={campata}
-          required={true}
-        />
-      </div>
-      <div className="mb-3 col-md-4">
-        <label htmlFor="ripiano" className="text-primary">
-          <strong>Ripiano</strong>
-        </label>
-        <input
-          type="text"
-          placeholder="Inserisci ripiano"
-          autoComplete="off"
-          name="text"
-          className="form-control rounded-0"
-          onChange={(e) => setRipiano(e.target.value)}
-          value={ripiano}
-          required={true}
-        />
-      </div>
-      <div className="mb-3 col-md-4">
-        <label htmlFor="cassetta" className="text-primary">
-          <strong>Cassetta</strong>
-        </label>
-        <input
-          type="text"
-          placeholder="Inserisci cassetta"
-          autoComplete="off"
-          name="text"
-          className="form-control rounded-0"
-          onChange={(e) => setCassetta(e.target.value)}
-          value={cassetta}
-          required={true}
-        />
-      </div>
-      <div className="mb-3 col-md-4">
-        <label htmlFor="annoImmatricolazione" className="text-primary">
-          <strong>Anno Immatricolazione</strong>
-        </label>
-        <input
-          type="text"
-          placeholder="Inserisci anno immatricolazione"
-          autoComplete="off"
-          name="number"
-          className="form-control rounded-0"
-          onChange={(e) => setAnnoImmatricolazione(e.target.value)}
-          value={annoImmatricolazione}
-          required={true}
-        />
-      </div>
+
+
       <div className="mb-3 col-md-4">
         <label htmlFor="marca" className="text-primary">
           <strong>Marca</strong>
@@ -252,6 +89,36 @@ function AggiungiVeicolo({ urlImmagine }) {
           className="form-control rounded-0"
           onChange={(e) => setVersione(e.target.value)}
           value={versione}
+          required={true}
+        />
+      </div>
+	   <div className="mb-3 col-md-4">
+        <label htmlFor="annoImmatricolazione" className="text-primary">
+          <strong>Anno Immatricolazione</strong>
+        </label>
+        <input
+          type="text"
+          placeholder="Inserisci anno immatricolazione"
+          autoComplete="off"
+          name="number"
+          className="form-control rounded-0"
+          onChange={(e) => setAnnoImmatricolazione(e.target.value)}
+          value={annoImmatricolazione}
+          required={true}
+        />
+      </div>
+	  	   <div className="mb-3 col-md-4">
+        <label htmlFor="annoImmatricolazione" className="text-primary">
+          <strong>Targa</strong>
+        </label>
+        <input
+          type="text"
+          placeholder="Inserisci targa"
+          autoComplete="off"
+          name="number"
+          className="form-control rounded-0"
+          onChange={(e) => setTarga(e.target.value)}
+          value={targa}
           required={true}
         />
       </div>
