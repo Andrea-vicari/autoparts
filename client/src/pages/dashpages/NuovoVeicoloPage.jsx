707885c9390d3f2d6f4a96a0eb7779b331ca3883
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2026-03-09 11:38:58 +0100

    Create NuovoVeicoloPage.jsx

---
+++ client/src/pages/dashpages/NuovoVeicoloPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from '../../Components/Common/Footer';
+import Navbar from '../../Components/Common/DashNav';
+import AggiungiImmagine from '../../Components/dashcomponents/AggiungiImmagine';
+
+
+function NuovoVeicoloPage() {
+  return (
+    <>
+    <Navbar />
+    <AggiungiImmagine />
+    <Footer />
+    </>
+  )
+}
+
+export default NuovoVeicoloPage
