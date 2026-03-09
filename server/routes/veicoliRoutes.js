be4618baa2735d7caa32feb8e5627050e803c1a8
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2026-03-09 10:20:43 +0100

    Implement file upload handling in veicoliRoutes
    Added file upload handling with Cloudinary integration.

--- server/routes/veicoliRoutes.js
+++ server/routes/veicoliRoutes.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const multer = require('multer')
 const cloudinary = require('cloudinary').v2;
-
-
-
 const router = express.Router();
+
+
+async function handleUpload(file) {
+
+    const res = await cloudinary.uploader.upload(file, {
+	folder:"Autoparts",
+    resource_type: "auto",
+    });
+    console.log(res)
+    return res;
+}
+
+const storage = new multer.memoryStorage();
+
+const upload = multer({
+  storage,
+});
 
 const {vediVeicoli} = require('../controllers/veicoliController');
 
