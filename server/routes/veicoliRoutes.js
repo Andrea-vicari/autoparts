752e4ae6a64d0e929d303b21237a77b72d36c71b
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2026-03-09 10:34:39 +0100

    Implement file upload route
    Add file upload endpoint with error handling

--- server/routes/veicoliRoutes.js
+++ server/routes/veicoliRoutes.js
@@ -20,6 +20,23 @@ const upload = multer({
   storage,
 });
 
+router.post("/upload", upload.single("my_file"), async (req, res) => {
+	try {
+	  const b64 = Buffer.from(req.file.buffer).toString("base64");
+	  let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
+
+	  const cldRes = await handleUpload(dataURI);
+
+	  res.json(cldRes);
+	} catch (error) {
+	  console.log(error);
+	  res.send({
+		message: error.message,
+	  });
+	}
+  });
+router.post('/crea-componente', creaComponente);
+
 const {vediVeicoli} = require('../controllers/veicoliController');
 
 // Get
