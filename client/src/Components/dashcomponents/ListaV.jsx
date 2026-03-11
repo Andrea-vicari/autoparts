5dde0d42a841395771409d9fe99f6c74143895a0
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2026-03-11 15:13:08 +0100

    Update ListaV.jsx

--- client/src/Components/dashcomponents/ListaV.jsx
+++ client/src/Components/dashcomponents/ListaV.jsx
@@ -24,24 +24,12 @@ const ListaV = ({ veicoli, loading }) => {
             : veicoli.map((data, index) => (
                 <tr key={index}>
                 <td><LigthBox immagine={data.urlImmagine}/></td>
-                <td className='pt-3'>
-                  <a href={'https://www.google.com/search?q=' + data.codice} target="_blank">{data.codice}</a>
 
-                </td>
-                <td className='pt-3'>{data.nome}</td>
-                <td className='pt-3'>{data.categoria}</td>
-                <td className='pt-3'>{data.condizione}</td>
                 <td className='pt-3'>{data.marca}</td>
                 <td className='pt-3'>{data.modello}</td>
                 <td className='pt-3'>{data.versione}</td>
                 <td className='pt-3'>{data.annoImmatricolazione}</td>
-                <td className='pt-3'>
-                  <p id="modello-componente" className="mb-0">SCAFFALE: {data.scaffale}</p>
-                  <p id="modello-componente" className="mb-0 py-0">CAMPATA: {data.campata}</p>
-                  <p id="modello-componente" className="mb-0">RIPIANO: {data.ripiano}</p>
-                  <p id="modello-componente" className="mb-0">CASSETTA: {data.cassetta}</p>
-                </td>
-                <td className='pt-3'>{data.peso} Kg</td>
+
                 <td className='pt-3'>
                   <Link to={`/modificacomponente/${data._id}`} state={data._id} type="button" className="btn btn-sm btn-outline-danger mx-1">
                     <i className='bi bi-zoom-in'></i>
